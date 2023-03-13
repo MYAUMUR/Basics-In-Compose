e495fa6582758005b46999c5d2e8e29399b45941
Pavlo Herasymchuk <pavloherasumchuc@gmail.com>
2023-03-13 16:55:29 +0200

    Add AffirmationApp module

---
+++ affirmations/src/main/java/com/herasymchuk/affirmations/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.herasymchuk.affirmations
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.herasymchuk.affirmations.ui.theme.BasicsInComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BasicsInComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AffirmationApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun AffirmationApp() {
+
+}
